3a3f06aae645cbe149bd1ea5bcc76d92d2856690
Peter Danenberg <pcd@roxygen.org>
2011-02-16 01:26:12 -0800

    debug/syslog

--- debug.scm
+++ debug.scm
@@ -1,12 +1,13 @@
 (module
  debug
- (debug trace)
- (import chicken scheme extras)
+ (debug debug/syslog make-syslog-port trace)
+ (import chicken scheme extras data-structures ports)
+ (import-for-syntax ports matchable)
+ (use syslog srfi-13)
 
  (define-syntax trace
    (er-macro-transformer
-    (lambda (expression rename compare)
-      (import matchable)
+    (lambda (expression rename compare) 
       (match-let (((_ f) expression))
         (let ((%set! (rename 'set!))
               (%lambda (rename 'lambda))
@@ -35,7 +36,34 @@
                   (,%apply ,%values return-values))))))))))
 
  (define-syntax debug
-  (syntax-rules ()
-    ((_ x ...)
-     (begin (write `((x ,x) ...) (current-error-port))
-            (newline (current-error-port)))))))
+   (syntax-rules ()
+     ((_ x ...)
+      (with-output-to-port
+          (current-error-port)
+        (lambda ()
+          (write `((x ,x) ...))
+          (newline))))))
+
+ (define default-priority (make-parameter prio/debug))
+ 
+ (define make-syslog-port
+   (case-lambda
+    (()
+     (make-syslog-port (default-priority)))
+    ((priority)
+     (let ((buffer ""))
+       (make-output-port
+        (lambda (scribendum)
+          (set! buffer (string-append/shared buffer scribendum)))
+        noop
+        (lambda () (syslog priority buffer)))))))
+    
+ (define-syntax debug/syslog
+   (er-macro-transformer
+    (lambda (expression rename compare)
+      `(let ((port (make-syslog-port)))
+         (with-error-output-to-port
+          port
+          (lambda ()
+            (debug ,@(cdr expression))
+            (flush-output port))))))))
