5a93c34f21d333408e40644a40c292e32baf2657
Peter Danenberg <pcd@roxygen.org>
2012-10-06 16:00:36 -0700

    Need srfi-13.

--- debug.scm
+++ debug.scm
@@ -1,7 +1,7 @@
 (module debug
   (debug debug? debug/syslog trace)
   (import chicken scheme extras data-structures ports srfi-13)
-  (import-for-syntax ports matchable)
-  (use syslog)
+  (import-for-syntax data-structures ports matchable srfi-13)
+  (use srfi-13 syslog)
 
   (include "debug-core.scm"))
