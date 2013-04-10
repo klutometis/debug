948714950b2c624555e132252a77a3c186b73207
Peter Danenberg <pcd@roxygen.org>
2013-04-09 18:56:05 -0700

    Fix debug/syslog with exports.

--- debug.scm
+++ debug.scm
@@ -1,5 +1,5 @@
 (module debug
-  (debug debug? debug/syslog trace)
+  (debug debug? debug/syslog default-priority make-syslog-port trace)
   (import chicken scheme extras data-structures ports srfi-13)
   (import-for-syntax data-structures ports matchable srfi-13)
   (use srfi-13 syslog)
