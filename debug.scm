b4e4a80c4deeea3108554edb2b5830fa07de8b77
Peter Danenberg <pcd@roxygen.org>
2013-09-13 06:50:42 -0700

    Spacing

--- debug.scm
+++ debug.scm
@@ -1,7 +1,22 @@
 (module debug
-  (debug debug? debug/syslog default-priority make-syslog-port trace)
-  (import chicken scheme extras data-structures ports srfi-13)
-  (import-for-syntax data-structures ports matchable srfi-13)
-  (use srfi-13 syslog)
+  (debug
+   debug?
+   debug/syslog
+   debug-priority
+   make-syslog-port
+   trace)
+
+  (import chicken
+          scheme
+          extras
+          data-structures
+          ports
+          srfi-13)
+  (import-for-syntax data-structures
+                     ports
+                     matchable
+                     srfi-13)
+  (use srfi-13
+       syslog)
 
   (include "debug-core.scm"))
